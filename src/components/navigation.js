2f887813966420bb89cce54e7fdfa5b00445bb75
Rina Rim <rina.rim803@gmail.com>
2023-01-08 15:38:07 -0800

    initialize App
    - add react router
    - create an Navigation component

---
+++ src/components/navigation.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Navigation = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Rina's Portfolio</li>
+        <li>About</li>
+        <li>Project</li>
+        <li>Resume</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
---
+++ src/pages/landing.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Landing = () => {
+  return (
+    <div>Welcome to my Portfolio site!</div>
+  )
+}
+
+export default Landing
