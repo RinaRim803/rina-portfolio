e608a63ece50c6ff31d4e6b689258201f96d3322
Rina Rim <rina.rim803@gmail.com>
2023-01-08 16:13:45 -0800

    Update navigation Css

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,14 +1,14 @@
 import React from "react";
-
+import style from "./navigation.module.css";
 const Navigation = () => {
   return (
-    <nav>
-      <ul>
-        <li>Rina's Portfolio</li>
-        <li>About</li>
-        <li>Project</li>
-        <li>Resume</li>
-        <li>Contact</li>
+    <nav className={style.nav}>
+      <span className={style.logo}>Rina's Portfolio</span>
+      <ul className={style.navRight}>
+        <li className={style.navItem}>About</li>
+        <li className={style.navItem}>Project</li>
+        <li className={style.navItem}>Resume</li>
+        <li className={style.navItem}>Contact</li>
       </ul>
     </nav>
   );
